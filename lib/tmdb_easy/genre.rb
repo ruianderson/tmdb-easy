154c8808c69179a3ae8cab23dad45ee3e84b5a3a
Bruno Henrique - Garu <squall.bruno@gmail.com>
2012-07-22 22:25:29 -0300

    add method genre_movies

--- lib/tmdb_easy/genre.rb
+++ lib/tmdb_easy/genre.rb
@@ -8,5 +8,14 @@ module TmdbEasy
       json_response = JSON(tmdb_response)
       genre_list = DeepOpenStruct.load(json_response).genres
     end
+
+    def self.genre_movies(genre_id, lang = 'en')
+      method_request = base_api_url + "genre/#{genre_id}/movies?api_key=#{@@api_key}&language=#{lang}"
+
+      uri = URI.parse(method_request)
+      tmdb_response = Net::HTTP.get_response(uri).body
+      json_response = JSON(tmdb_response)
+      movies_per_genre = DeepOpenStruct.load(json_response).results
+    end
   end
 end
