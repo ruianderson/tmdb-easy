8c967fe670882c3a211a292f42793b54a64dc380
Bruno Henrique - Garu <squall.bruno@gmail.com>
2012-07-24 00:04:06 -0300

    settings

--- api_examples_genre.rb
+++ api_examples_genre.rb
@@ -1,6 +1,6 @@
 require "./lib/tmdb_easy"
 
-TmdbEasy::Base.api_key = "f89492d116074ec340563aad0fcea52c"
+TmdbEasy::Base.api_key = "your_api_key"
 genre_list = TmdbEasy::Genre.list
 movies_per_genre = TmdbEasy::Genre.genre_movies 18
 
--- api_examples_search.rb
+++ api_examples_search.rb
@@ -1,6 +1,6 @@
 require "./lib/tmdb_easy"
 
-TmdbEasy::Base.api_key = "f89492d116074ec340563aad0fcea52c"
+TmdbEasy::Base.api_key = "your_api_key"
 movies = TmdbEasy::Search.movie "transformer"
 people = TmdbEasy::Search.person "bruno"
 companies = TmdbEasy::Search.company "net"
