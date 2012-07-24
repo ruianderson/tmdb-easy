88fb746ce8612a868dac848e6a1ae07d6f884d61
Brenno Costa <brennolncosta@gmail.com>
2012-07-24 18:50:09 -0300

    better legibility on require

--- lib/tmdb_easy.rb
+++ lib/tmdb_easy.rb
@@ -8,10 +8,8 @@ require 'uri'
 require 'json'
 require 'deepopenstruct'
 
-#Load files in the correct order, not in alphabetical natural order. :)
-required_files = ['base.rb',
-                  'search.rb',
-                  'genre.rb']
+# Load files in the correct order, not in alphabetical natural order. :)
+required_files = %w(base.rb search.rb genre.rb)
 
 required_files.each do |file|
   require File.expand_path(File.join(File.dirname(__FILE__), 'tmdb_easy', file))
