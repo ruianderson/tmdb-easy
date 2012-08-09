09d4f6321e98bad97f5a962bbd2e4f591cf711db
Bruno Henrique - Garu <squall.bruno@gmail.com>
2012-08-09 19:09:10 -0300

    tests beginning

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,1 +1,14 @@
 require 'spec_helper'
+
+describe TmdbEasy::Base do
+  let(:base) {TmdbEasy::Base}
+
+  it 'should allow setting and getting of api_key' do
+    base.api_key = 'ihih7ye7y7398y3498493jhehj3'
+    base.api_key.should == 'ihih7ye7y7398y3498493jhehj3'
+  end
+
+  it 'must return base api url' do
+    base.base_api_url.should == 'http://api.themoviedb.org/3/'
+  end
+end
---
+++ spec/search_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe TmdbEasy::Search do
+  let(:search) { TmdbEasy::Search }
+end
