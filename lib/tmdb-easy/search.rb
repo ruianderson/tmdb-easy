3fde5029e45480c6fac0683a228bc987df20eeeb
Bruno Henrique - Garu <squall.bruno@gmail.com>
2012-08-10 14:45:51 -0300

    bug: space between the url

--- lib/tmdb-easy/search.rb
+++ lib/tmdb-easy/search.rb
@@ -1,21 +1,21 @@
 module TmdbEasy
   class Search < Base
     def self.movie(keyword, lang = 'en', adult = false)
-      action_request = base_api_url + "search/movie?api_key=#{@@api_key}&query=#{keyword}&language=#{lang}&include_adult=#{adult}"
+      action_request = base_api_url + "search/movie?api_key=#{@@api_key}&query=#{keyword}&language=#{lang}&include_adult=#{adult}".sub(/ /, "+")
 
       response = api_response(action_request)
       movies = DeepOpenStruct.load(response).results
     end
 
     def self.person(keyword, adult = false)
-      action_request = base_api_url + "search/person?api_key=#{@@api_key}&query=#{keyword}&include_adult=#{adult}"
+      action_request = base_api_url + "search/person?api_key=#{@@api_key}&query=#{keyword}&include_adult=#{adult}".sub(/ /, "+")
 
       response = api_response(action_request)
       people = DeepOpenStruct.load(response).results
     end
 
     def self.company(keyword)
-      action_request = base_api_url + "search/company?api_key=#{@@api_key}&query=#{keyword}"
+      action_request = base_api_url + "search/company?api_key=#{@@api_key}&query=#{keyword}".sub(/ /, "+")
 
       response = api_response(action_request)
       companies = DeepOpenStruct.load(response).results
